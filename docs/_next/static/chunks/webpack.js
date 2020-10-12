8f179f5e74e07db61c59fb385c74a479716345a2
Logan Deal <logan.deal27@gmail.com>
2020-10-11 21:30:31 -0500

    Built example blog site

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "4eb0e843b667fcc94208";
+/******/ 	var hotCurrentHash = "23229f894902f420e879";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- docs/_next/static/z8LrsHDUjKDvJwJqZWX3b/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fe619a664d57fdbbc5594.css","static\u002Fchunks\u002Fpages\u002Findex-b2db7e379a1d7b1aabd0.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ed1b06dc12d6382f8fbd.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
