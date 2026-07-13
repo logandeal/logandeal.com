6dba9cbed1d67dc8f15d37e762e320c9060ac0cd
Logan Deal <53441579+logandeal@users.noreply.github.com>
2026-07-12 19:22:35 -0500

    Update tabs

--- getAllPosts.js
+++ getAllPosts.js
@@ -1,10 +1,10 @@
 function importAll(r) {
-   return r.keys().map((fileName) => ({
-      link: fileName.substr(1).replace(/\/index\.mdx$/, ""),
-      module: r(fileName)
-   }));
+  return r.keys().map((fileName) => ({
+    link: fileName.substr(1).replace(/\/index\.mdx$/, ''),
+    module: r(fileName),
+  }));
 }
 
 export const posts = importAll(
-   require.context("./pages/blog/", true, /\.mdx$/)
+  require.context('./pages/blog/', true, /\.mdx$/),
 );
